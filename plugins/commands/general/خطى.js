023c72ee27901fa27f3bba0d53cec92e74ced338
sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>
2026-01-17 19:38:34 +0200

    Update and rename streak.js to خطى.js

--- plugins/commands/general/خطى.js
+++ plugins/commands/general/خطى.js
@@ -1,10 +1,10 @@
 const config = {
-    name: "streak",
+    name: "خطى",
     description: "Day counting...",
     usage: "[start/set/relapse/stop]",
     cooldown: 5,
     permissions: [0, 1, 2],
-    credits: "XaviaTeam"
+    credits: "ᏕᎥᏁᎨᎧ"
 }
 
 const langData = {
