f4aaa878b608b68891e5c8d1fcfdc0276ff698a1
sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>
2026-01-17 20:02:59 +0200

    Update and rename threadnoti.js to شمار.js

--- plugins/commands/group/شمار.js
+++ plugins/commands/group/شمار.js
@@ -1,10 +1,10 @@
 const config = {
-    name: "threadnoti",
+    name: "شمار",
     permissions: [1, 2],
     description: "Turn on/off thread notification for yourself",
     usage: "[on/off]",
     cooldown: 5,
-    credits: "XaviaTeam"
+    credits: "ᏕᎥᏁᎨᎧ"
 }
 
 const langData = {
