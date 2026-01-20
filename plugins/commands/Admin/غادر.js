8a78f751ffdef8484db66a1987101a60c015f7ad
sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>
2026-01-20 21:14:29 +0200

    Update غادر.js

--- plugins/commands/Admin/غادر.js
+++ plugins/commands/Admin/غادر.js
@@ -11,151 +11,52 @@ const config = {
 };
 
 const langData = {
-    vi_VN: {
-        noThreadToOut: "Không có nhóm nào để rời.",
-        invalidThreadIDs: "ID nhóm không hợp lệ.",
-        confirm: "React 👍 để xác nhận.",
-        moderator: "Quản trị Bot",
-        out: "",
-        successOut: "",
-        failOut: "Không thể rời khỏi nhóm:\n{fail}",
-        error: "Đã có lỗi xảy ra, vui lòng thử lại sau.",
-    },
-    en_US: {
-        noThreadToOut: "There is no group to leave.",
-        invalidThreadIDs: "Invalid group IDs.",
-        confirm: "React 👍 to confirm.",
-        moderator: "Bot Moderator",
-        out: "",
-        successOut: "",
-        failOut: "Unable to leave group:\n{fail}",
-        error: "An error has occurred, please try again later.",
-    },
-    ar_SY: {
-        noThreadToOut: "لا توجد مجموعة لتغادر.",
-        invalidThreadIDs: "معرفات المجموعة غير صالحة.",
-        confirm: "تفاعل ب 👍 للتأكيد.",
-        moderator: "مشرف الروبوت",
-        out: "",
-        successOut:"",
-        failOut: "غير قادر على مغادرة المجموعة:\n{fail}",
-        error: "حصل خطأ. الرجاء المحاوله مرة اخرى.",
-    },
+    vi_VN: {},
+    en_US: {},
+    ar_SY: {},
 };
 
 function out(threadID) {
     return new Promise((resolve) => {
-        global.api.removeUserFromGroup(global.botID, threadID, (err) => {
-            if (err) {
-                console.error(err);
-                return resolve(null);
-            }
+        global.api.removeUserFromGroup(global.botID, threadID, () => {
             resolve(true);
         });
     });
 }
 
-async function verifyAccess({ message, getLang, eventData, data }) {
-    try {
-        const { reaction, userID } = message;
-        if (reaction != "👍") return;
-
-        let threadIDs = eventData.threadIDs;
-
-        const isHavingCurrentThreadID = threadIDs.some(
-            (threadID) => threadID == message.threadID
-        );
-        if (isHavingCurrentThreadID) {
-            threadIDs = threadIDs.filter(
-                (threadID) => threadID != message.threadID
-            );
-            threadIDs.push(message.threadID);
-        }
-
-        let authorName = data?.user?.info?.name || getLang("moderator");
-
-        const fail = [];
-        for (const threadID of threadIDs) {
-            await message.send(
-                {
-                    body: getLang("out", { authorName }),
-                    mentions: [{ tag: authorName, id: userID }],
-                },
-                threadID
-            );
-
-            const result = await out(threadID);
-            if (result == null) fail.push(threadID);
-
-            await global.utils.sleep(500);
-        }
-
-        const sendTarget =
-            isHavingCurrentThreadID &&
-            !fail.some((threadID) => threadID == message.threadID)
-                ? userID
-                : null;
-
-        const successCount = threadIDs.length - fail.length;
-
-        await message.send(getLang("successOut", { successCount }), sendTarget);
-        if (fail.length > 0)
-            await message.send(
-                getLang("failOut", { fail: fail.join("\n") }),
-                sendTarget
-            );
-
-        return;
-    } catch (e) {
-        console.error(e);
-        return message.send(getLang("error"));
-    }
-}
-
-async function onCall({ message, args, getLang }) {
+async function onCall({ message, args }) {
     try {
         const input = args[0]?.toLowerCase();
         const threadIDs = [];
 
-        if (input == "all") {
-            const threadList = (
-                (await global.api.getThreadList(100, null, ["INBOX"])) || []
-            ).filter(
+        if (input === "all") {
+            const threadList =
+                (await global.api.getThreadList(100, null, ["INBOX"])) || [];
+
+            const groups = threadList.filter(
                 (thread) =>
-                    thread.threadID != message.threadID &&
+                    thread.threadID !== message.threadID &&
                     thread.isGroup &&
                     thread.isSubscribed
             );
 
-            if (threadList.length == 0)
-                return message.reply(getLang("noThreadToOut"));
-
-            threadIDs.push(...threadList.map((thread) => thread.threadID));
+            threadIDs.push(...groups.map((t) => t.threadID));
         } else if (args.length > 0) {
             const inputThreadIDs = args
-                .map((threadID) => threadID.replace(/[^0-9]/g, ""))
-                .filter((arg) => arg.length >= 16 && !isNaN(arg));
-
-            if (inputThreadIDs.length == 0)
-                return message.reply(getLang("invalidThreadIDs"));
+                .map((id) => id.replace(/[^0-9]/g, ""))
+                .filter((id) => id.length >= 16 && !isNaN(id));
 
             threadIDs.push(...inputThreadIDs);
         } else {
             threadIDs.push(message.threadID);
         }
 
-        return message
-            .reply(getLang("confirm"))
-            .then((_) => _.addReactEvent({ threadIDs, callback: verifyAccess }))
-            .catch((e) => {
-                if (e.message) {
-                    console.error(e.message);
-                    return message.reply(getLang("error"));
-                }
-            });
+        for (const threadID of threadIDs) {
+            await out(threadID);
+            await global.utils.sleep(300);
+        }
     } catch (e) {
         console.error(e);
-        return message.reply(getLang("error"));
     }
 }
 
