118636fc81a0400de051f98c091e85ac3c1486a2
sadtyhdhynrqm-spec <sadtyhdhynrqm@gmail.com>
2026-01-30 22:58:28 +0200

    Update المطور.js

--- plugins/commands/general/المطور.js
+++ plugins/commands/general/المطور.js
@@ -51,7 +51,7 @@ async function onCall({ message, getLang }) {
         const { threadID } = message;
 
         const imageURL =
-            "https://i.ibb.co/1GkLbB3r/1768714709999.jpg";
+            "https://i.ibb.co/27RrNnX1/file-000000003da0722fafdd0c28522ad11c.png";
 
         const imgStream = (
             await axios.get(imageURL, { responseType: "stream" })
